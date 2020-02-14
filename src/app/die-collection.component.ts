67bfe36346d3e96bcd6fcf4845225550f186fbca
Perpetka <perpetka@gmail.com>
2020-02-14 19:04:09 +0100

    add die fix

--- src/app/die-collection.component.ts
+++ src/app/die-collection.component.ts
@@ -37,5 +37,6 @@ export class DieCollectionComponent {
       this.rolledDice.push( new RolledDie(6, "aqua") );
     else
       this.rolledDice.push( new RolledDie(max, "orange"));
+    this.calculateSum();
   }
 }

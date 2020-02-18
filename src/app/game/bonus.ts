7c3428f1e5deb7f266abddbec84314236a30f6e2
Perpetka <perpetka@gmail.com>
2020-02-19 00:13:13 +0100

    bonuses WIP

--- src/app/game/bonus.ts
+++ src/app/game/bonus.ts
@@ -1,3 +1,5 @@
+import {RolledGameDie} from './game-dice';
+
 export class Bonus
 {
  name: string;
@@ -5,10 +7,27 @@ export class Bonus
 
 export class ExtraDie extends Bonus
 {
+  die: RolledGameDie;
   
+  constructor( bonusDie: RolledGameDie)
+  {
+    super();
+    this.die = bonusDie;
+  }
 }
 
 export class Action extends Bonus
-{
-  
+{  
+}
+
+export class ReRollAction extends Action
+{  
+}
+
+export class PlayOneMoreDie extends Action
+{  
+}
+
+export class RetrieveDie extends Action
+{  
 }
--- src/app/game/game-dice.ts
+++ src/app/game/game-dice.ts
@@ -1,9 +1,19 @@
 import {RolledDie } from '../dice';
 
 
-export class RolledGameDie extends RolledDie{
+export class RolledGameDie extends RolledDie
+{
+  isWildcardValue: boolean;
+
    isWildcardColor() 
    { 
      return this.color === "white"; 
+   }
+
+   constructor(color: string, value: number, isWildcardValue: boolean)
+   {
+     super(6, color);
+     this.value = value;
+     this.isWildcardValue = isWildcardValue;
    }
 }

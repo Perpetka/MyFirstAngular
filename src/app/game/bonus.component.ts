f02ef8555ceb3240cb5dfe92266dd5eb4e7595ce
Perpetka <perpetka@gmail.com>
2020-02-19 01:08:10 +0100

    bonuses styling

--- src/app/game/bonus.component.ts
+++ src/app/game/bonus.component.ts
@@ -1,14 +1,33 @@
 import {Component, Input} from '@angular/core';
-import {Bonus, Action, ExtraDie} from './bonus';
+import {Bonus, Action, ExtraDieBonus} from './bonus';
 
 @Component({
   selector: "bonus",
-  template: `<div *ngIf="bonus" [className]="getClass()">{{getIcon()}}</div>`,
-  styles: [".field {float: left; height:20px; width: 20px; cursor:pointer; border: solid 1px; padding: 1px; margin: 2px; }"]
+  template: `<div *ngIf="bonus" [ngClass]="
+  {
+    'bonus': true,
+    'action' : isAction(),
+    'die' : isExtraDie()
+  }
+  "
+  [style.background-color] = "getColor()"
+  >{{getIcon()}}</div>`,
+  styles: [".bonus {position:absolute; font-size: xx-small; bottom: -7px; left: 9px; width: 6px;} ",
+   ".action {color: white;}"]
 })
 export class BonusComponent
 {
   @Input() bonus: Bonus;
+
+  isAction(): boolean {return this.bonus instanceof Action; }
+  isExtraDie() : boolean {return  this.bonus instanceof ExtraDieBonus; }
+
+  getColor()
+  {
+    if( this.isExtraDie() )
+      return (<ExtraDieBonus>this.bonus).die.color;
+    return "black";
+  }
   
   getClass()
   {
@@ -17,6 +36,9 @@ export class BonusComponent
 
   getIcon()
   {
-    return "B";
+    if( this.isAction() )
+      return "A";
+    else
+      return "B";
   }
 }
--- src/app/game/value-field.component.ts
+++ src/app/game/value-field.component.ts
@@ -5,8 +5,8 @@ import {BonusComponent} from './bonus.component';
 
 @Component({
   selector: "value-field",
-  template: `<div class="field" [style.background-color]="field.color" (click)="onclick()">{{field.value}}</div><bonus [bonus]="field.bonus"></bonus>`,
-  styles: [".field {float: left; height:20px; width: 20px; cursor:pointer; border: solid 1px; padding: 1px; margin: 2px; }"]
+  template: `<div class="field" [style.background-color]="field.color" (click)="onclick()">{{field.value}}<bonus [bonus]="field.bonus"></bonus></div>`,
+  styles: [".field {float: left; height:20px; width: 20px; cursor:pointer; border: solid 1px; padding: 1px; margin: 2px; position:relative; }"]
 })
 export class ValueFieldComponent
 {
